c11199ea0dd64deefdff035a82e5c5eed9f76792
James Castro <jcastro61@hotmail.com>
2019-01-26 08:36:56 -0500

    Adding Home and Login

--- routes.py
+++ routes.py
@@ -1,6 +1,15 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from models import db, User
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://pgadmin:jcc88tru@localhost/learningflask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+
+db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -12,6 +21,56 @@ def index():
 def about():
 	return render_template('about.html')
 
-	
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+  form = SignupForm()
+
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+      return 'Success!'
+
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template("login.html", form=form)
+        else:
+            email = form.email.data
+            password = form.password.data
+
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session['email'] = form.email.data
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('login'))
+
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+def logout():
+  session.pop('email', None)
+  return redirect(url_for('index'))
+
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+  return render_template("home.html")
+
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000,debug=True)
